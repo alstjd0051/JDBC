0103685e5fa6cb81527c6eba61d3e8c83113078e
dev_addictied <70365399+alstjd0051@users.noreply.github.com>
2021-02-16 19:36:17 +0900

    Add files via upload

---
+++ 02_Service_JDBC_Template/src/member/model/dao/MemberDao.java
@@ -0,0 +1,69 @@
+package member.model.dao;
+
+import static common.JDBCTemplate.close;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import member.model.vo.Member;
+
+public class MemberDao {
+
+	/*
+	* Dao
+	 * 3. PreparedStatement객체 생성(미완성쿼리)
+	 * 3.1 ?물음표 값대입
+	 * 4. 실행 DML(excuteUpdate)의 경우 정수형을리턴 DQL(executeQuery)의경우 ReseultSet으로 리턴
+	 * 4.1 ResultSet -> Java객체 옮겨담기
+	 * 5. 자원반납(생성역순 rset - pstmt)
+	 */ 
+	public List<Member> selectAll(Connection conn) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = "select * from member order by enroll_date desc";
+		List<Member> list = null;
+		
+		try {
+			//3. PreparedStatement객체 생성(미완성쿼리)
+			pstmt = conn.prepareStatement(sql);
+			//3.1 ?물음표 값대입
+			
+			//4. 실행 DML(excuteUpdate)의 경우 정수형을리턴 DQL(executeQuery)의경우 ReseultSet으로 리턴
+			rset = pstmt.executeQuery();
+			//4.1 ResultSet -> Java객체 옮겨담기
+			list = new ArrayList<>();
+			while(rset.next()) {
+				String memberId = rset.getString("member_id");
+				String password = rset.getString("password");
+				String memberName = rset.getString("member_name");
+				String gender = rset.getString("gender");
+				int age = rset.getInt("age");
+				String email = rset.getString("email");
+				String phone = rset.getString("phone");
+				String address = rset.getString("address");
+				String hobby = rset.getString("hobby");
+				Date enrollDate = rset.getDate("enroll_date");
+				Member member = new Member(memberId, password, memberName, gender, age, email, phone, address, hobby, enrollDate);
+				list.add(member);
+			}
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			//5. 자원반납(생성역순 rset - pstmt)
+			close(rset);
+			close(pstmt);
+		}
+		return list;
+	}
+
+	public int insertMember(Connection conn, Member member) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
